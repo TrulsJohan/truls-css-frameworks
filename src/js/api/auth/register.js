c2a8de32da0e8bf93aa479c64d2705007c7b6436
fillip husebø <felipehusebo@gmail.com>
2024-09-24 16:19:58 +0200

    added register feature,

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,8 +1,35 @@
+import {API_AUTH_REGISTER} from "../constants.js";
+
+
 export async function register({
   name,
   email,
   password,
-  bio,
-  banner,
-  avatar,
-}) {}
+
+}) {
+  try {
+    const response = await fetch(API_AUTH_REGISTER, {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }else{
+      console.log("created account" + name)
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error during registration:', error);
+    throw error;
+  }
+}
