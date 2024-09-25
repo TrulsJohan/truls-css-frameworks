7bd943a481a433c571896b74c4d2cba431657209
Truls Johan Jacobsen-Lamøy <truls@Trulss-MacBook-Pro.local>
2024-09-25 12:36:56 +0200

    exported getkey function

--- src/js/api/auth/key.js
+++ src/js/api/auth/key.js
@@ -1,1 +1,11 @@
-export async function getKey(name) {}
+export async function getKey() {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+        return accessToken;
+    } else {
+        console.error("Access denied: No access token found.");
+
+        window.location.href = "/login";
+        return null;
+    }
+}
